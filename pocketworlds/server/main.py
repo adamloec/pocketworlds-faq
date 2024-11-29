99718a1ff6df3d7f80f1071797c5449be7782059
Adam <adamloeckle@gmail.com>
2024-11-28 23:51:00 -0600

    testing api endpoints for vercel

--- pocketworlds/server/main.py
+++ pocketworlds/server/main.py
@@ -17,7 +17,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://pocketworlds-ah72e4vue-adamloeckle-gmailcoms-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
